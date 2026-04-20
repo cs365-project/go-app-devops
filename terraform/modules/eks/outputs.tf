44115056e41429393a345cb03e5921e74a2c5ec0
กิตติภณ คำนวล <yiyophone8266@gmail.com>
2026-04-20 13:47:08 +0700

    feat: setup Terraform remote backend and fix Learner Lab IAM roles
    - Add bootstrap-remote-state.sh to create S3 bucket + DynamoDB table
    - Add .gitignore for Terraform artifacts
    - Fix EKS module to look up LabEksClusterRole/LabEksNodeRole by regex
      (Learner Lab prefixes role names with a generated string)
    - Remove OIDC provider resource (not permitted in Learner Lab)

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -10,10 +10,3 @@ output "cluster_certificate_authority_data" {
   value = aws_eks_cluster.main.certificate_authority[0].data
 }
 
-output "oidc_provider_arn" {
-  value = aws_iam_openid_connect_provider.eks.arn
-}
-
-output "oidc_provider_url" {
-  value = aws_iam_openid_connect_provider.eks.url
-}
